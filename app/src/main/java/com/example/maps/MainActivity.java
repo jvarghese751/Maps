17b8469a9e453925d8b3b4863bd2d9e4920fe62c
Joel Varghese <Varghese-J2@ulster.ac.uk>
2023-11-27 13:09:22 +0000

    Initial commit

---
+++ app/src/main/java/com/example/maps/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.maps;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        @SuppressLint({"MissingInflatedId", "LocalSuppress"})
+        Button maps_btn = (Button) findViewById(R.id.maps_btn);
+        maps_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, MapsActivity.class);
+                startActivity(i);
+            }
+        });
+    }
+}
